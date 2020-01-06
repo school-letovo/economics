0ff3fbe6d5f0425df334378e9eda2b346c8d56e6
Vladimir Gurovic <gurovic@gmail.com>
2020-01-06 17:39:42 +0300

    student_list.html ant test view all users (/problems/students)

--- problems/urls.py
+++ problems/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     # ex: /polls/
     path('', views.all_problem_list, name='all_problem_list'),
+    path('students', views.all_student_list, name='all_student_list'),
 ]
--- problems/views.py
+++ problems/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 from .models import Problem
 # Create your views here.
@@ -7,3 +8,8 @@ def all_problem_list(request):
     problems = Problem.objects.all()
     context = {'problems': problems,}
     return render(request, 'problems/problem_list.html', context)
+
+def all_student_list(request):
+    students = User.objects.all()
+    context = {'students': students,}
+    return render(request, 'problems/student_list.html', context)
