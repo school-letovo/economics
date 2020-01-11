bd49f5a116b0e0d8583a39f251f4a57ac8bb11f4
Vladimir Gurovic <gurovic@gmail.com>
2020-01-11 07:22:25 +0300

    show only students in teacher_index

--- problems/urls.py
+++ problems/urls.py
@@ -5,7 +5,5 @@ from . import views
 urlpatterns = [
     # ex: /polls/
     path('', views.index, name='index'),
-    path('problems', views.all_problem_list, name='all_problem_list'),
-    path('students', views.all_student_list, name='all_student_list'),
     path('assign', views.assign, name='assign'),
 ]
--- problems/views.py
+++ problems/views.py
@@ -7,19 +7,9 @@ from .models import Assignment, Problem, Topic
 
 # Create your views here.
 
-def all_problem_list(request):
-    problems = Problem.objects.all()
-    context = {'problems': problems,}
-    return render(request, 'problems/problem_list.html', context)
-
-def all_student_list(request):
-    students = User.objects.all()
-    context = {'students': students,}
-    return render(request, 'problems/student_list.html', context)
-
 def index(request):
     if request.user.groups.filter(name='teachers').exists():
-        students = User.objects.all()
+        students = User.objects.filter(groups__name='students')
         topics = Topic.objects.all()
         problems = dict()
         for topic in topics:
