3c5d08cddb3a212a3df074d85022f89f24ee8960
Vladimir Gurovic <gurovic@gmail.com>
2020-10-26 13:47:12 +0300

    closed #184: bug fix

--- problems/views.py
+++ problems/views.py
@@ -671,7 +671,7 @@ def failed_tests(request, student_id, testset_pk):
         positive_result = TestSubmit.objects.filter(assignment__person=student, problem=problem, answer_autoverdict=True).count()
         if positive_result == 0 and negative_result > 0:
             answer.append(problem)
-
+    return render(request, "problems/testset_result.html", {'tests': answer})
 
 def create_user(request):
     if request.POST:
