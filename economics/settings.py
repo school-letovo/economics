832d87acff80e46a13b89c1dd558bde33a1aa68b
OlegBezr <obezrukavnikov@gmail.com>
2020-12-06 01:02:24 +0300

    mysql migration

--- economics/settings.py
+++ economics/settings.py
@@ -77,13 +77,26 @@ WSGI_APPLICATION = 'economics.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'h33347c_veconomics',
+        'USER': 'h33347c_oleg',
+        'PASSWORD': 'oleg_pass',
+        'HOST': '91.235.129.81',
+        'PORT': '3306'
     }
 }
 
+# 185.174.174.220
+# 91.235.129.81
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
